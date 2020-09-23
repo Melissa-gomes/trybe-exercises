f2cd326663fbedc271c7cbd69944042e0b8a42d6
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-23 11:54:32 -0300

    exercicios de forms

---
+++ bloco_6/dia_1/script.js
@@ -0,0 +1,14 @@
+function estados(){
+    let estadosBr= ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ]
+    for(let i= 0; i< estadosBr.length; i +=1){
+        let todos= estadosBr[i];
+        let local = document.querySelector("#estado")
+        let opcoes= document.createElement("option");
+        let acao= local.appendChild(opcoes);
+        let result= acao.innerHTML=todos;
+    }
+}
+let a = document.querySelector("#estado");
+a.addEventListener("click", estados);
+
+
