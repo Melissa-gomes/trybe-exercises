cf5d8b37c9def1a3cfbf4b26ff30ec9e7a97ef41
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-29 12:32:02 -0300

    exercicios inacabados

---
+++ bloco7/7_2/ex3.js
@@ -0,0 +1,30 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const turno = {
+  turno:'manhã'
+}
+Object.assign(lesson2, turno);
+
+console.log(lesson2);
+
+console.log(Object.keys(lesson1));
+
+console.log(lesson1.length);
