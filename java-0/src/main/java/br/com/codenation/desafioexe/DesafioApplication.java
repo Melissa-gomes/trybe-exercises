18b5048fd0fab11d705ea5c7c0e5aab6b7df18cc
Melissa Gomes <mel.ge171201@gmail.com>
2021-02-22 13:44:47 -0300

    exercicios de java fibonacci

---
+++ java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -0,0 +1,25 @@
+package br.com.codenation.desafioexe;
+
+import java.util.List;
+
+public class DesafioApplication {
+
+	public static List<Integer> fibonacci(int numero;) {
+
+		if (numero < 2) {
+			return numero;
+		} else {
+		  return fibonacci(numero -1) + fibonacci(numero -2)
+		}
+
+		public static void main(String[] args) {
+      for (int index = 0; i < 15; index ++) {
+      System.out.print( DesafioApplication.fibonacci(i) + " ");
+      }
+	}
+
+	public static Boolean isFibonacci(Integer a) {
+		return null;
+	}
+
+}
